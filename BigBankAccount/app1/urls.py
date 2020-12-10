bea7967b917978fd59f826c674685327977b517b
uchennak <uchennakamalu@gmail.com>
2020-12-09 21:30:46 -0500

    removed sub-module

---
+++ BigBankAccount/app1/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path     
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('process_money/',views.process_money),
+    path('reset/',views.reset),
+    path('no_more_coins',views.no_more_coins),
+    path('add_coins',views.add_coins),
+    path('display_login',views.display_login),  
+    path('display_register',views.display_register),
+    path('display_invest',views.display_invest),
+    path('invest',views.invest),
+    path('withdraw',views.withdraw),
+    path('login',views.login),
+    path('register',views.register),
+    path('logout',views.logout),
+]
